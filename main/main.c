3253f7eb68e576d2cff7b64a92e29c6f26f5fa83
Gustavo Fontana Bertoldi <gustavobertoldi20@gmail.com>
2025-03-03 13:18:04 -0300

    Structurig the project

--- main/main.c
+++ main/main.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h> //on linux
+//using windows: #include <windows.h>
+
+typedef struct {
+    int id;
+    char name[50];
+    float cost;
+}Product;
+
+typedef struct {
+    Product product;
+    int quantitie;
+}Kart;
+
+void infoProduct(Product product);
+void menu();
+void registerProduct();
+void listProduct();
+void visualizeKart ();
+Product takeProductByCode (int code);
+int * inTheKart (int code);
+void buyProducts ();
+
+static int product_count = 0;
+static int kart_count = 0;
+static Kart kart[50];
+static Product products[50];
 
 int main(){
-
+    menu();
 
     return 0;
+}
+
+void infoProduct(Product product){
+//TBD
+}
+void menu(){
+//TBD
+}
+void registerProduct(){
+//TBD
+}
+void listProduct(){
+//TBD
+}
+void visualizeKart (){
+//TBD
+}
+Product takeProductByCode (int code){
+//TBD
+}
+int * inTheKart (int code){
+//TBD
+}
+void buyProducts (){
+//TBD
 }
